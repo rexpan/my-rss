99a89480a1066afcf273e3aacc6ffb109ba78308
Rex Pan <napxer@gmail.com>
2020-02-19 09:55:10 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,88 +1,46 @@
-// @ts-check
-const {writeFileSync, createWriteStream, accessSync} = require("fs");
+"use strict";
+
 const path = require("path");
-const cheerio = require("cheerio");
-const {fetchAtom} = require("./fetchAtom");
+
+const simpleGit = require('simple-git/promise');
+
+const { Processor:P1 } = require("./techrum.vn/Processor");
+const { Processor:P2 } = require("./udemycoupon.learnviral.com/Processor");
+
+const rssDir = path.resolve(__dirname, "../rss/");
+const sleepTime = 2 * 60 * 1000;
+
 main();
-
 async function main(){
-    const [error, xs, meta] = await fetchAtom(`http://9gag-rss.com/api/rss/get?code=9GAGHot&format=1`);
-    if (error != null) {
-        console.error(error);
-        return;
-    }
+    console.log("begin squash");
+    await squash();
+    console.log("end squash\n");
 
-    const items = xs.map(parseItem);
-    writeFileSync("out.json", JSON.stringify({items, meta}, null, 2));
+    const ps = [
+        P1,
+        P2,
+    ].map(Processor => new Processor({ rssDir }));
 
-    for (let item of items) {
-        if (item.video != null) {
-            const video = `./media/${item.id}.mp4`;
-            const [error] = await downloadFile(item.video, path.resolve(video));
-            if (error) console.error(error);
-            else item.video = video;
-        } else {
-            const img = `./media/${item.id}.jpg`;
-            const [error] = await downloadFile(item.img, path.resolve(img));
-            if (error) console.error(error);
-            else item.img = img;
+    let i = 0;
+    while (true) {
+        console.info("begin process %d (%s)", i, new Date());
+        for (const p of ps) {
+            const [err, updated] = await p.start();
+            if (err != null) { console.error(p.feedUrl, err); continue; }
         }
+        console.info("end process %d", i);
+
+        i++;
+
+        await sleep(sleepTime);
     }
-    writeFileSync("out.html",
-        items.map(item => `
-        <html>
-        <head>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-        </head>
-        <body>
-        <div class="container">
-            <div class="card mb-3"><div class="card-body">
-                <h4 class="card-title"><a href="${item.link}">${item.title}</a></h4>
-                <div>${item.video
-                    ? `<video src="${item.video}"></video>`
-                    : `<img src="${item.img}" class="img-fluid">`
-                }</div>
-            </div></div>
-        </div>
-        </body>
-        </html>`).join(""));
 }
 
-
-function parseItem({title, description, guid, link}){
-    const $ = cheerio.load(description);
-    const img = $("img").attr("src");
-    const video = $(`video source`).attr("src");
-    const id = guid.replace("https://9gag.com/gag/", "");
-    return (video != null
-        ? { id, guid, title, link, video }
-        : { id, guid, title, link, img   }
-    );
+async function sleep(n){
+    return new Promise(resolve => setTimeout(() => resolve(), n));
 }
 
-const http = require('http');
-const https = require('https');
-
-function downloadFile(url, fileName){
-    const httpGet = url.startsWith("https") ? https.get : http.get;
-
-    return new Promise(resolve => {
-        try {
-            accessSync(fileName);
-            resolve([null]);
-            return;
-        } catch(e) {
-            console.log("begin downloadFile", url, fileName);
-        }
-
-        const file = createWriteStream(fileName);
-        const request = httpGet(url, (response) => response.pipe(file));
-        request.on("error", function(error){ resolve([error]) })
-        file.on("finish", function(){
-            resolve([null]);
-        });
-        file.on("error", function(error){
-            resolve([error]);
-        });
-    });
+function squash() {
+    const git = simpleGit(path.resolve(__dirname, rssDir));
+    return git.reset(["--hard", "dev"])
 }
