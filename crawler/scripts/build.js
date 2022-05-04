57bab650d1bb9c6087bc20f4da28451bd5b869bc
Rex Pan <napxer@gmail.com>
2022-05-04 09:13:36 +0700

    update package.json

--- crawler/scripts/build.js
+++ crawler/scripts/build.js
@@ -9,18 +9,16 @@ try {
         platform: 'node',
         bundle: true,
         format: 'esm',
-        target: 'node17.5.0',
         // keepNames: true,
         legalComments: 'none',
         minify: true,
         external: [
-            // native
-            'pg-native',
             // dynamic require
-            ...('undici dotenv feedparser @kwsites/file-exists debug'.split(' ')),
+            ...('undici feedparser simple-git'.split(' ')),
         ],
     });
 } catch(err) {
+
     process.exit(1);
 }
 
